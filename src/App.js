92ed785c4aafebb7d9534db983207a241e944b55
furm walker <walkerfurman@yahoo.com>
2023-01-19 13:47:46 -0500

    added react router and changed nav

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Wrapper from './components/Wrapper/Wrapper';
 import Home from './components/HomePage.js/Home';
+import TV from './pages/TV';
+
+
 
 function App() {
   return (
-    <Wrapper>
-      <section>
-        <Home></Home>
-      </section>
+    <BrowserRouter>
+      <Wrapper>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="tv" element={<TV />} />
+        </Routes>
+      </Wrapper>
+    </BrowserRouter>
+    // <section>
 
-    </Wrapper>
+    //   <Home></Home>
+    // </section>
   );
 }
 
--- src/components/UI/Slider.js
+++ src/components/UI/Slider.js
@@ -3,6 +3,10 @@ import "slick-carousel/slick/slick-theme.css";
 import Carousel from "react-elastic-carousel";
 import './Slider.css'
 import free from '../assets/free.jpg'
+import grim from '../assets/grim.jpg'
+import frozen from '../assets/frozen.jpg'
+import stone from '../assets/stone.jpg'
+import violent from '../assets/violent.jpg'
 // import Slider from "react-slick";
 
 
@@ -10,26 +14,29 @@ import free from '../assets/free.jpg'
 const SimpleSlider = () => {
 
     const items = [
-        { id: 1, title: 'item #1' },
-        { id: 2, title: 'item #2' },
-        { id: 3, title: 'item #3' },
-        { id: 4, title: 'item #4' },
-        { id: 5, title: 'item #5' },
-        { id: 6, title: 'item #6' },
-        { id: 7, title: 'item #7' },
+        { id: 1, title: 'item #1', src: free },
+        { id: 2, title: 'item #2', src: grim },
+        { id: 3, title: 'item #3', src: frozen },
+        { id: 4, title: 'item #4', src: stone },
+        { id: 5, title: 'item #5', src: violent },
+        { id: 6, title: 'item #6', src: free },
+        { id: 7, title: 'item #7', src: grim },
+        { id: 8, title: 'item #7', src: frozen },
+        { id: 9, title: 'item #7', src: free },
+        { id: 10, title: 'item #7', src: stone },
     ]
 
 
-    const productInfo = items.map(({ id, title, src }) => {
+    const productInfo = items.map(({ id, src }) => {
         return (
             <div id={id} className="slide">
                 <img
                     src={src}
                     alt='A movie'
                 />
-                <div>
+                {/* <div>
                     <p>{title}</p>
-                </div>
+                </div> */}
             </div>
         )
     })
@@ -39,7 +46,7 @@ const SimpleSlider = () => {
         // { width: 700, itemsToShow: 2 },
         // { width: 900, itemsToShow: 3 },
         // { width: 1550, itemsToShow: 3 },
-        { width: 1600, itemsToShow: 6 },
+        { width: 1600, itemsToShow: 8 },
     ];
 
     return (
