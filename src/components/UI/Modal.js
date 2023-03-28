6f88f4a0546943b5f3a6c025dd98188a4c48e491
furm walker <walkerfurman@yahoo.com>
2023-03-28 09:18:42 -0400

    added modal component and react-player

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,11 @@
+import './Modal.css'
+
+const Modal = () => {
+    return (
+        <div>
+            <h1>its a modal</h1>
+        </div>
+    )
+}
+
+export default Modal
--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,5 +1,8 @@
-import { useState, useEffect } from "react"
 import "./Image.css"
+import { useState, useEffect } from "react"
+// import Modal from "../components/UI/Modal"
+
+// import YouTube from 'react-youtube';
 
 const Movies = () => {
     const [pictures, setPictures] = useState([])
@@ -19,15 +22,30 @@ const Movies = () => {
         catch (err) { }
     }
 
+    // const fetchData2 = async () => {
+    //     try {
+    //         const req = await fetch('https://api.themoviedb.org/3/movie/804150/videos?api_key=b8f14a6a8c65baef33f77a05cfe2bf47&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1&with_genres=27&with_watch_monetization_types=flatrate', {
+    //             method: 'GET',
+    //             headers: {
+    //                 'Content-Type': 'application/json'
+    //             }
+    //         })
+    //         const data = await req.json()
+    //         console.log(data)
+    //     }
+    //     catch (err) { }
+    // }
+
 
     useEffect(() => {
         fetchData()
+        // fetchData2()
     }, [])
 
 
     const movieInfo = pictures.map(({ id, poster_path }) => {
         return (
-            <div key={id} id={id}>
+            <div key={id} id={id} >
                 <img
                     src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
                     alt='A movie'
@@ -40,6 +58,7 @@ const Movies = () => {
     return (
         <div className="picture-container">
             {movieInfo}
+            {/* <YouTube videoId="cGIVKAGQNKM" play={true} loading='lazy' /> */}
         </div>
 
     )
