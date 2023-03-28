f555cc74a16724ea6c949f584039811b68e1d9e2
furm walker <walkerfurman@yahoo.com>
2023-03-28 14:16:27 -0400

    added icons to nav

--- src/components/MainNavigation/MainNav.js
+++ src/components/MainNavigation/MainNav.js
@@ -1,26 +1,26 @@
 import './Mainnav.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faVideo } from '@fortawesome/free-solid-svg-icons'
+import { faTv } from '@fortawesome/free-solid-svg-icons'
+
 
 const Mainnav = () => {
   return (
     <nav className="nav-container">
       <div className="nav-logo">
-        <a href='/'><p>React Movies</p></a>
+        <a href='/'><h2>React Movies</h2></a>
       </div>
       <ul className="nav-links" id="nav-links">
         <li className="nav-link"><a href="/">Home</a></li>
-        <li className="nav-link"><a href="/movies">Movies</a></li>
-        <li className="nav-link"><a href="/tv">TV</a></li>
+        <li className="nav-link"><a href="/movies"><FontAwesomeIcon icon={faVideo} />Movies</a></li>
+        <li className="nav-link"><a href="/tv"><FontAwesomeIcon icon={faTv} />TV</a></li>
         <li className="nav-link"><a href="/trending">Trending</a></li>
         <div className="hamburger" id="hamburger-icon">
           <i className="fa-solid fa-bars"></i>
         </div>
-        {/* <form>
-          <input placeholder='search'></input>
-        </form> */}
       </ul>
-      <div>
+      <div className='icon-container'>
         <a href='/search'>
           <FontAwesomeIcon className="nav-icon" icon={faMagnifyingGlass} beatFade />
         </a>
